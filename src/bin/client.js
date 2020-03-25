9276852aa2a37a7963711d95ac3e3e6f9c5bb082
Robert Luis <robert.l.bluemel@gmail.com>
2020-03-26 00:48:45 +0100

    export module

--- src/bin/client.js
+++ src/bin/client.js
@@ -4,30 +4,32 @@ const fetch = require('node-fetch');
 const newEngine = require('@comunica/actor-init-sparql').newEngine;
 
 //Input variables  ->   process.argv[2]
-const source = "http://localhost:8080/t1/t1-1/person1/profile.ttl"
-const webid = source + '#me'
-const query = `
-PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-SELECT ?o WHERE { graph ?g {
-    ?s foaf:knows ?o. }
-}`;
+//const source = "http://localhost:8080/t1/t1-1/person1/profile.ttl"
+//const webid = source + '#me'
+//const query = `
+//PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+//SELECT ?o WHERE { graph ?g {
+//    ?s foaf:knows ?o. }
+//}`;
 
 
-// ## Initialising the request
-requestHandler(source, webid, query).then(res => {
-    console.log(res);
-})
-.catch(err=> {console.error('Error! Server is not responding \n' + err) });
+//// ## Initialising the request
+//requestHandler(source, webid, query).then(res => {
+//    console.log(res);
+//})
+//.catch(err=> {console.error('Error! Server is not responding \n' + err) });
 
 
-// ## Request handler function
-async function requestHandler(url, webid, query) {
-    const newSource = await enforceAC(source, webid, query);
-    if (newSource.length > 0) {
-        const results = await executeQuery(query, source);
-        callCleaner(newSource)
-        return results
-    } else { return [] }
+module.exports = {
+    // ## Request handler function
+    requestHandler: async function (url, webid, query) {
+        const newSource = await enforceAC(source, webid, query);
+        if (newSource.length > 0) {
+            const results = await executeQuery(query, source);
+            callCleaner(newSource)
+            return results
+        } else { return [] }
+    }
 }
 
 // ## Calling the AC-Interface
